a9207946ef6603907c9a851d0cbbd8fd7bfdfd0d
HOJUN2404 <rlaghwns1995@naver.com>
2021-11-30 15:09:52 +0900

    스프링부트 연결 테스트

---
+++ src/main/java/com/example/demo/UpbitTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbitTestApplication.class, args);
+	}
+
+}
+  
